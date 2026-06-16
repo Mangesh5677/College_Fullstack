cd7996604e6dfa9602d51d54a678a62e38cc203b
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-16 15:39:43 +0530

    upadate

--- TypeScript/annotation.js
+++ TypeScript/annotation.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let name;
-name = "Herry";
-name = "John";
-console.log(name);
+let heightInCentimeters;
+let isActive;
+const names = ["Alice", "Bob", "Charlie"];
+console.log(names);
 //# sourceMappingURL=annotation.js.map
--- TypeScript/annotation.ts
+++ TypeScript/annotation.ts
@@ -1,3 +1,6 @@
 let name : string;
 let heightInCentimeters : number;
 let isActive : boolean;
+
+const names:string[] = ["Alice", "Bob", "Charlie"];
+console.log(names);
