743eaa455e5a37a32790b40142bfce60ea51e5a1
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-16 14:48:16 +0530

    upadate

--- TypeScript/example1.ts
+++ TypeScript/example1.ts
@@ -1,6 +1,8 @@
-const firstName = "John";
+/* const firstName = "John";
 
 console.log(firstName.length);
 
 let a: number = 100;
-let x = 200;
+let x: number = 200; */
+
+let bestSong = Math.random() > 0.5 ? "Song A" : "Song B";
