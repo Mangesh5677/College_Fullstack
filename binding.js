3a689b1210350e97f9cb81110ab31c56a6f6f02b
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-16 10:13:05 +0530

    upadate

--- binding.js
+++ binding.js
@@ -1,13 +1,14 @@
 export var color = "red";
 export let name = "John";
+export const magicNumber = 45;
 
 export function sum(num1, num2){
     return num1 + num2;
 }
 
 export class Rectangle{
-    constructor(length, breadth){
+    constructor(length, width){
         this.length = length;
-        this.breadth = breadth;
+        this.width = width;
     }
 }
