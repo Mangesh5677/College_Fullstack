351113a5821ec1e9dd124d1c36afd237ab2d2757
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-16 15:54:11 +0530

    upadate

--- TypeScript/annotation.js
+++ TypeScript/annotation.js
@@ -1,8 +1,14 @@
 "use strict";
+/* let name : string;
+let heightInCentimeters : number;
+let isActive : boolean;
+
+const names:string[] = ["Alice", "Bob", "Charlie"];
+console.log(names); */
 Object.defineProperty(exports, "__esModule", { value: true });
-let name;
-let heightInCentimeters;
-let isActive;
-const names = ["Alice", "Bob", "Charlie"];
-console.log(names);
+let SayHello;
+SayHello = (name) => {
+    return 'some string';
+};
+SayHello('Hello');
 //# sourceMappingURL=annotation.js.map
--- TypeScript/annotation.ts
+++ TypeScript/annotation.ts
@@ -1,6 +1,14 @@
-let name : string;
+/* let name : string;
 let heightInCentimeters : number;
 let isActive : boolean;
 
 const names:string[] = ["Alice", "Bob", "Charlie"];
-console.log(names);
+console.log(names); */
+
+let SayHello : (name: string) => string;
+
+SayHello = (name) => {
+    return 'some string';
+};
+
+SayHello('Hello');
