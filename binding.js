9d1c372915552169d0f48af9f64f68e9527d17a0
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-16 10:11:35 +0530

    upadate

--- binding.js
+++ binding.js
@@ -4,3 +4,10 @@ export let name = "John";
 export function sum(num1, num2){
     return num1 + num2;
 }
+
+export class Rectangle(){
+    constructor(length, breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+}
