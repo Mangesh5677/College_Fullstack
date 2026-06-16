fc870acae6a622a2f3f9931932bf379b8de70502
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-16 15:02:09 +0530

    upadate

--- TypeScript/example1.js
+++ TypeScript/example1.js
@@ -1,8 +1,8 @@
+"use strict";
 /* const firstName = "John";
 
 console.log(firstName.length);
 
 let a: number = 100;
 let x: number = 200; */
-
 let bestSong = Math.random() > 0.5 ? "Song A" : "Song B";
