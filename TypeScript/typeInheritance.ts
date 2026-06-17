96b54d6448f3a52741d199c2ed50f12d1d62e462
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-17 10:01:40 +0530

    upadate

--- TypeScript/typeInheritance.ts
+++ TypeScript/typeInheritance.ts
@@ -2,6 +2,15 @@
     name: "William Shakespeare",
     born: 2000,
 }; */
+/* const PoetLater:typeof Poet = {
+    name: "John Doe",
+    born: 2001,
+};
+
+const YetAnotherPoet:typeof Poet = {
+    name: "Shreeman",
+    born: 2010,
+}; */
 type Poet = {
     name: string;
     born: number;
