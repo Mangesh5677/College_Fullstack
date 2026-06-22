62fc558de3f8e02b8a560ea2d2fd13432bdf6c0f
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-22 14:27:34 +0530

    update

---
+++ JavaOOPs/src/First.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Character");
+        char x = sc.next().charAt(0);
+        System.out.println(x);
+    }
+}
