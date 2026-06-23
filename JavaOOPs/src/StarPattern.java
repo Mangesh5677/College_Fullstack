54c8a72fefd13888cec3f2795dfd0b00101c1575
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-23 11:20:48 +0530

    update

--- JavaOOPs/src/StarPattern.java
+++ JavaOOPs/src/StarPattern.java
@@ -3,11 +3,8 @@ public class StarPattern {
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
-            }
-            for(int n=5;n>=i+1;n--){
-                System.out.print("_ ");
-            }
-            for(int n=5;n>=i+1;n--){
+             }
+            for(int row=1;row<=2*(5-i);row++){
                 System.out.print("_ ");
             }
             for(int j=1;j<=i;j++){
