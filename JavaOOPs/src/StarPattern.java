36d53590e46ac6900b1cc8267011c765db9ee171
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-23 11:46:11 +0530

    update

--- JavaOOPs/src/StarPattern.java
+++ JavaOOPs/src/StarPattern.java
@@ -1,20 +1,25 @@
+class Pattern1{
+void Ptr(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print("* ");
+        }
+        for(int row=1;row<=2*(n-i);row++){
+            System.out.print("_ ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print("* ");
+        }
+        System.out.println("");
+    }
+}
+}
 public class StarPattern {
     public static void main(String[] args) {
-        int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-             }
-            for(int row=1;row<=2*(5-i);row++){
-                System.out.print("_ ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println("");
-        }
-
-
+        Pattern1 p = new Pattern1();
+        p.Ptr(5);
+        System.out.println("   ");
+        p.Ptr(6);
     }
 }
 //output:
